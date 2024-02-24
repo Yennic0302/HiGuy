77324f8dcf6cfee36ef0b58182714ff30a2168c8
Yender45 <yender.yr45@gmail.com>
2024-02-24 17:59:33 -0400

    updated

--- client/next.config.js
+++ client/next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [{ key: "Cross-Origin-Opener-Policy", value: "unsafe-none" }],
-      },
-    // ];
-  },
   images: {
     remotePatterns: [
       {
