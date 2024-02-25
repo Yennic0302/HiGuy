30e25efd58493e5210dee47637186aa6a10f7b2f
Yender45 <yender.yr45@gmail.com>
2024-02-25 11:01:08 -0400

    push

--- client/src/app/chat_components/Chatlist/ChatList/index.tsx
+++ client/src/app/chat_components/Chatlist/ChatList/index.tsx
@@ -23,7 +23,7 @@ function ChatList() {
       {!contactsPage && !searchPage && (
         <>
           <ChatListHeader />
-          <h2 className="text-2xl  w-full text-[--text-primary] p-4 bg-[--search-input-container-background]">
+          <h2 className="text-2xl pt-16 sm:pt-0  w-full text-[--text-primary] p-4 bg-[--search-input-container-background]">
             Chats
           </h2>
           <List />
