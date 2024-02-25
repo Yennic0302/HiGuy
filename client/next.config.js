cb786358b1b227299399a0da9773642e6bb9ecc8
Yender45 <yender.yr45@gmail.com>
2024-02-25 02:36:42 -0400

    push

--- client/next.config.js
+++ client/next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: "http",
         hostname: "localhost",
       },
+      {
+        protocol: "http",
+        hostname: "hi-guy.vercel.app/",
+      },
     ],
   },
 };
